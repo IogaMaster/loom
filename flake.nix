64db6393b66f132b1e705faf672004cf44ea30bd
IogaMaster <iogamastercode@gmail.com>
2025-10-18 17:38:26 -0600

    :tada: Initial Commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: function nixpkgs.legacyPackages.${system});
+    in rec {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            clang-tools
+            clang
+            gdb
+
+            # Build system
+            just
+            meson
+            ninja
+            pkg-config
+            fd
+          ];
+
+          buildInputs = with pkgs; [
+            sdl3
+            sdl3-image
+            sdl3-ttf
+
+            # Runtime deps for sdl3
+            libpng
+            freetype
+            dbus
+            wayland
+          ];
+
+          shellHook = ''
+            export CC=clang
+            export CXX=clang++
+          '';
+
+        };
+      });
+    };
+}
