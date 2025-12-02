c9e10a7e877df362622d8c41df3064d979157394
IogaMaster <iogamastercode@gmail.com>
2025-12-01 21:26:05 -0700

    feat(modules/debugger): embed using c header
    We cannot rely on the clang extension #embed. The lua file is kept in
    order to allow for lua lsp completion of the module.

--- src/modules/debugger/debugger.cpp
+++ src/modules/debugger/debugger.cpp
@@ -1,12 +1,12 @@
 #include "sol/state.hpp"
 #include <loom/utils/lua_module.hpp>
 
-namespace {
-const std::string embeddedData = {
-#embed "debugger.lua" // NOLINT
-};
-} // namespace
+#define DEBUGGER_LUA_IMPLEMENTATION
+#include "debugger_lua.h"
 
 extern "C" void register_loom_module(sol::state &lua) {
-  lua.script(embeddedData);
+  dbg_setup(lua.lua_state(), "loom.debugger", "", nullptr, nullptr);
+  auto debugger = loom::utils::create_lua_module(lua, "debugger");
+  auto module = lua.require("loom.debugger", nullptr);
+  lua["loom"]["debugger"] = module;
 }
