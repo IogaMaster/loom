9128d691d5dcd7ee0844936663c9c1985604a4de
IogaMaster <iogamastercode@gmail.com>
2025-12-06 16:18:58 -0700

    feat(modules/loom_log): implement remaining log levels

--- src/modules/loom_log.rs
+++ src/modules/loom_log.rs
@@ -3,11 +3,36 @@ use mlua::prelude::*;
 
 pub fn init_loom_log(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
+
+    let trace = lua.create_function(|_, msg: String| -> LuaResult<()> {
+        trace!("[Lua] {}", msg);
+        Ok(())
+    })?;
+    exports.set("trace", trace)?;
+
+    let debug = lua.create_function(|_, msg: String| -> LuaResult<()> {
+        debug!("[Lua] {}", msg);
+        Ok(())
+    })?;
+    exports.set("debug", debug)?;
+
     let info = lua.create_function(|_, msg: String| -> LuaResult<()> {
         info!("[Lua] {}", msg);
         Ok(())
     })?;
+    exports.set("info", info)?;
 
-    exports.set("info", info);
+    let warn = lua.create_function(|_, msg: String| -> LuaResult<()> {
+        warn!("[Lua] {}", msg);
+        Ok(())
+    })?;
+    exports.set("warn", warn)?;
+
+    let error = lua.create_function(|_, msg: String| -> LuaResult<()> {
+        error!("[Lua] {}", msg);
+        Ok(())
+    })?;
+    exports.set("error", error)?;
+
     Ok(exports)
 }
