84f108d147fbe9d1230b4da669901b52b3f0adee
IogaMaster <iogamastercode@gmail.com>
2025-12-07 11:53:06 -0700

    refactor(core/game_loop): remove unneeded mut's

--- src/core/game_loop.rs
+++ src/core/game_loop.rs
@@ -1,14 +1,14 @@
 use std::time::{Duration, Instant};
 
 pub fn game_loop<T: FnMut(), F: FnMut(f32)>(ticks_per_second: &f32, mut tick: T, mut frame: F) {
-    let mut time_step = 1.0 / ticks_per_second;
+    let time_step = 1.0 / ticks_per_second;
     let mut delta_time = 0.0;
     let mut accumulator = 0.0;
 
     let mut last_time = Instant::now();
 
     loop {
-        let mut current_time = Instant::now();
+        let current_time = Instant::now();
         delta_time = (current_time - last_time).as_secs_f32();
         last_time = current_time;
         accumulator += delta_time;
