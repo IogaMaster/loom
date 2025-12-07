ce2f92ff3766dc5518eaed627a2db0d3f09e2730
IogaMaster <iogamastercode@gmail.com>
2025-12-07 10:36:23 -0700

    refactor(core/game_loop): accumulator based timing
    Two timers is more complex, less readable and more cpu intensive. Now we
    accumulate frame times until that time is one tick, then subtract tick
    time from it and exec a tick.

--- src/core/game_loop.rs
+++ src/core/game_loop.rs
@@ -1,19 +1,21 @@
 use std::time::{Duration, Instant};
 
 pub fn game_loop<T: FnMut(), F: FnMut(f32)>(ticks_per_second: &f32, mut tick: T, mut frame: F) {
-    let mut tick_delta = 1.0 / ticks_per_second;
-    let mut tick_timer = Instant::now();
-
+    let mut time_step = 1.0 / ticks_per_second;
     let mut delta_time = 0.0;
+    let mut accumulator = 0.0;
+
+    let mut last_time = Instant::now();
 
     loop {
-        let frame_timer = Instant::now();
-        if tick_timer.elapsed().as_secs_f32() > tick_delta {
+        let mut current_time = Instant::now();
+        delta_time = (current_time - last_time).as_secs_f32();
+        last_time = current_time;
+        accumulator += delta_time;
+        while accumulator >= time_step {
             tick();
-            tick_delta = 1.0 / ticks_per_second;
-            tick_timer = Instant::now();
+            accumulator -= time_step;
         }
-        frame(delta_time);
-        delta_time = frame_timer.elapsed().as_secs_f32();
+        frame(delta_time)
     }
 }
